e61e60b02bd9ae3261bf991f3db4c0bddc5260ac
devcon <devcon@admantium.com>
2018-10-03 10:22:40 +0200

    Simple test case with always true assertion

---
+++ test.js
@@ -0,0 +1,7 @@
+#!/usr/local/bin/mocha
+var assert = require('assert');
+describe("MysteryLunch", () => {
+   it("should print 'Welcome to the mystery lunch!' when started", () => {
+      assert.equal(1,1);
+   })
+})
