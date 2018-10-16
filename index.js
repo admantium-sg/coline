6f87c5f3fed3b51488714f671d1d609caf396ff6
devcon <devcon@admantium.com>
2018-10-16 08:08:02 +0200

    Applied lib 'standard' to refine source coude layout

--- index.js
+++ index.js
@@ -1,176 +1,174 @@
 #!/usr/local/bin/node
-const fs = require('fs');
-const EventEmitter = require('events');
+const fs = require('fs')
+const EventEmitter = require('events')
 
 class Handler extends EventEmitter {}
 
 class LunchEvent {
-   constructor() {
-      this.answers = [];
-      this.title = '';
-      this.date = '';
-      this.participants = '';
-   
-      this.nextQuestion = () => {
-         if(this.answers.length === 3) return false;
-         return this.constructor.getCreationQuestions()[this.answers.length];
-      }
+  constructor () {
+    this.answers = []
+    this.title = ''
+    this.date = ''
+    this.participants = ''
 
-      this.answerQuestion = (data) => {
-         if (this.answers.length === 3) throw new LunchEventError('All questions are already answered');
-         this.answers.push(data);
-      }
+    this.nextQuestion = () => {
+      if (this.answers.length === 3) return false
+      return this.constructor.getCreationQuestions()[this.answers.length]
+    }
 
-      this.isComplete = () => {
-         return this.answers.length === 3;
-      }
+    this.answerQuestion = (data) => {
+      if (this.answers.length === 3) throw new LunchEventError('All questions are already answered')
+      this.answers.push(data)
+    }
 
-      this.printData = () => {
-         return [this.answers[0],
-            this.answers[1],
-            this.answers[2]];
-      }
+    this.isComplete = () => {
+      return this.answers.length === 3
+    }
 
-      this.getSuccessMessage = () => {
-         [this.title, this.date, this.participants] = this.answers;
-            return "Thank you! The event is registered.";
-           
-      }
-   }
+    this.printData = () => {
+      return [this.answers[0],
+        this.answers[1],
+        this.answers[2]]
+    }
 
-   static getInterfaceMenu() { 
-      return ["Welcome to managing events. What do you want to do?",
-         "- (C) Create new event",
-         "- (S) Show all events"];
-   }
+    this.getSuccessMessage = () => {
+      [this.title, this.date, this.participants] = this.answers
+      return 'Thank you! The event is registered.'
+    }
+  }
 
-   static getCreationQuestions() {
-      return ["What is the name of the event?",
-         "When is the event going to happen?",
-         "Who is participating?"];
-   }
+  static getInterfaceMenu () {
+    return ['Welcome to managing events. What do you want to do?',
+      '- (C) Create new event',
+      '- (S) Show all events']
+  }
+
+  static getCreationQuestions () {
+    return ['What is the name of the event?',
+      'When is the event going to happen?',
+      'Who is participating?']
+  }
 }
 
 class LunchEventError extends Error {};
 
 class MysteryLunch {
-   constructor() {
-      this.lunch_events = [];
+  constructor () {
+    this.lunch_events = []
 
-      this.stdout = process.stdout;
-      this.stdin = process.stdin;
-      this.stdin.setEncoding('utf-8');
-      this.handler = new Handler();
+    this.stdout = process.stdout
+    this.stdin = process.stdin
+    this.stdin.setEncoding('utf-8')
+    this.handler = new Handler()
 
-      this.prompt = "$: ";
-      this.rprompt = "$> ";
-      this.nl = "\r\n";
+    this.prompt = '$: '
+    this.rprompt = '$> '
+    this.nl = '\r\n'
 
-      this.logfile = './mystl.log';
-      fs.unlink('./mystl.log', (err) => {
-         if(err) throw err;
-      });
-      this.setup_commands();
+    this.logfile = './mystl.log'
+    fs.unlink('./mystl.log', (err) => {
+      if (err) throw err
+    })
+    this.setupCommands()
 
-      this.contextObject = null;
-      this.question_counter = 0;
-   }
-   
-   start() {
-      this.write_line('Mystery Lunch Planner'); 
-      this.write_prompt();
+    this.contextObject = null
+    this.question_counter = 0
+  }
 
-      this.stdin.on('data',  (raw_data) => {
-         this.stdin.pause();
-         this.handle_command(raw_data)
-         this.stdin.resume();
-      });
-   }
+  start () {
+    this.writeLine('Mystery Lunch Planner')
+    this.writePrompt()
 
-   handle_command(raw_data) {
-      let cmd  = raw_data.toString().trim();
-      this.log(cmd + this.nl);
+    this.stdin.on('data', (rawData) => {
+      this.stdin.pause()
+      this.handleCommand(rawData)
+      this.stdin.resume()
+    })
+  }
 
-      // If context objectt exists, emit event creation with cmd as input
-      // ELSE send cmd as Event code 
-      // FINALLY default to echo
-      if(this.contextObject) {
-         this.handler.emit('event_creation', cmd);
-      } else if (! this.handler.emit(cmd, cmd)) {
-         this.handler.emit('default', cmd);
-      };
-      this.write_prompt();
-   }
-          
-   setup_commands() {
-      this.handler.on('default', (cmd) => {
-         this.write_result("'" + cmd + "'");
-      });
-      this.handler.on('M', () => {
-         LunchEvent.getInterfaceMenu().forEach(item => this.write_result(item));
-      })
-      this.handler.on('S', () => {
-         for(let event of this.lunch_events) {
-            event.printData().foreach(item => this.write_line("--- " + item));
-         }  
-      });
-      this.handler.on('C', () => {
-         //Bind new lunch event to context object
-         this.contextObject = new LunchEvent();
-         //Print first message of context object
-         this.write_question(this.contextObject.nextQuestion());
-      })
-      this.handler.on('event_creation', (cmd) => {    
-          this.contextObject.answerQuestion(cmd);     
-         // IF incomplete print out next question
-         // ELSE Add created object and reset context object 
-         if(!this.contextObject.isComplete()) {
-            this.write_question(this.contextObject.nextQuestion());
-         } 
-         else {
-            this.addEvent(this.contextObject);
-            this.write_line(this.contextObject.getSuccessMessage());
-            this.contextObject = null;
-         }
-      })
-      this.handler.on('exit', () => {
-         this.stdin.destroy();
-      });
-   }
+  handleCommand (rawData) {
+    let cmd = rawData.toString().trim()
+    this.log(cmd + this.nl)
 
-   addEvent(lunch_event = Object) {
-      this.lunch_events.push(lunch_event);
-   }
+    // If context objectt exists, emit event creation with cmd as input
+    // ELSE send cmd as Event code
+    // FINALLY default to echo
+    if (this.contextObject) {
+      this.handler.emit('event_creation', cmd)
+    } else if (!this.handler.emit(cmd, cmd)) {
+      this.handler.emit('default', cmd)
+    };
+    this.writePrompt()
+  }
 
-   log(output) {
-      fs.appendFileSync(this.logfile, output, (err) => {
-         if(err) throw err;
-      });
-   }
+  setupCommands () {
+    this.handler.on('default', (cmd) => {
+      this.writeResult("'" + cmd + "'")
+    })
+    this.handler.on('M', () => {
+      LunchEvent.getInterfaceMenu().forEach(item => this.writeResult(item))
+    })
+    this.handler.on('S', () => {
+      for (let event of this.lunch_events) {
+        event.printData().foreach(item => this.writeLine('--- ' + item))
+      }
+    })
+    this.handler.on('C', () => {
+      // Bind new lunch event to context object
+      this.contextObject = new LunchEvent()
+      // Print first message of context object
+      this.writeQuestion(this.contextObject.nextQuestion())
+    })
+    this.handler.on('event_creation', (cmd) => {
+      this.contextObject.answerQuestion(cmd)
+      // IF incomplete print out next question
+      // ELSE Add created object and reset context object
+      if (!this.contextObject.isComplete()) {
+        this.writeQuestion(this.contextObject.nextQuestion())
+      } else {
+        this.addEvent(this.contextObject)
+        this.writeLine(this.contextObject.getSuccessMessage())
+        this.contextObject = null
+      }
+    })
+    this.handler.on('exit', () => {
+      this.stdin.destroy()
+    })
+  }
 
-   write_line(cmd) {
-      let output = cmd + this.nl;
-      this.stdout.write(output);
-      this.log(output);
-   }
+  addEvent (lunch_event = Object) {
+    this.lunch_events.push(lunch_event)
+  }
 
-   write_question(question) {
-      let output = this.rprompt + question + this.nl;
-      this.stdout.write(output);
-      this.log(output);
-   }
+  log (output) {
+    fs.appendFileSync(this.logfile, output, (err) => {
+      if (err) throw err
+    })
+  }
 
-   write_prompt() {
-      let output = this.prompt;
-      this.stdout.write(output);
-      this.log(output);
-   }
+  writeLine (cmd) {
+    let output = cmd + this.nl
+    this.stdout.write(output)
+    this.log(output)
+  }
 
-   write_result(cmd)  {
-      let output = this.rprompt + cmd + this.nl
-      this.stdout.write(output);
-      this.log(output);
-   }
+  writeQuestion (question) {
+    let output = this.rprompt + question + this.nl
+    this.stdout.write(output)
+    this.log(output)
+  }
+
+  writePrompt () {
+    let output = this.prompt
+    this.stdout.write(output)
+    this.log(output)
+  }
+
+  writeResult (cmd) {
+    let output = this.rprompt + cmd + this.nl
+    this.stdout.write(output)
+    this.log(output)
+  }
 }
 
-module.exports = {MysteryLunch, LunchEvent, LunchEventError};
+module.exports = { MysteryLunch, LunchEvent, LunchEventError }
--- test.js
+++ test.js
@@ -1,111 +1,109 @@
 #!/usr/local/bin/mocha
 // Prepare merge with branch A_event_emitter_redesign
-var assert = require('chai').assert;
-var should = require('chai').should();
-var expect = require('chai').expect;
-var test_stdout = require('test-console').stdout;
-var test_stdin = require('mock-stdin').stdin();
-var fs = require('fs');
-const MysteryLunch = require('./index.js').MysteryLunch;
-const LunchEvent = require('./index.js').LunchEvent;
-const LunchEventError = require('./index.js').LunchEventError;
-var mystl = new MysteryLunch();
+const assert = require('chai').assert
+const should = require('chai').should()
+const testStdout = require('test-console').stdout
+const testStdin = require('mock-stdin').stdin()
+const MysteryLunch = require('./index.js').MysteryLunch
+const LunchEvent = require('./index.js').LunchEvent
+const LunchEventError = require('./index.js').LunchEventError
+const mystl = new MysteryLunch()
 
-describe("Class LunchEvent", () => {
-   var test_event = new LunchEvent();
-   it("Should have three questions", () => {
-      LunchEvent.getCreationQuestions().should.have.lengthOf(3);
-      test_event.nextQuestion().should.be.equal('What is the name of the event?')
-   })
-   it("Should be incomplete when I only answer two questions", () => {
-      test_event.answerQuestion('Mystery Lunch 1');
-      test_event.answerQuestion('2018-10-13');
-      test_event.isComplete().should.be.false;
-   })
-   it("Should then show the correct third question 'Who is participating?'", () => {
-      test_event.nextQuestion().should.be.equal('Who is participating?');
-   })
-   it("Should be complete after answering three questions", () => {
-      test_event.answerQuestion('Sebastian, Janine');
-      test_event.isComplete().should.be.true;
-   })
-   it("Should throw an error when I want to add a fourth answer", () => {
-      // See Mocha Documentation - needs binding to be invoked correctly
-      test_event.answerQuestion.bind(test_event).should.throw(LunchEventError);
-   })
-   it("Should print the given answers", () => {
-      test_event.printData()[1].should.be.equal('2018-10-13');
-   })
+describe('Class LunchEvent', () => {
+  var test_event = new LunchEvent()
+  it('Should have three questions', () => {
+    LunchEvent.getCreationQuestions().should.have.lengthOf(3)
+    test_event.nextQuestion().should.be.equal('What is the name of the event?')
+  })
+  it('Should be incomplete when I only answer two questions', () => {
+    test_event.answerQuestion('Mystery Lunch 1')
+    test_event.answerQuestion('2018-10-13')
+    test_event.isComplete().should.be.false
+  })
+  it("Should then show the correct third question 'Who is participating?'", () => {
+    test_event.nextQuestion().should.be.equal('Who is participating?')
+  })
+  it('Should be complete after answering three questions', () => {
+    test_event.answerQuestion('Sebastian, Janine')
+    test_event.isComplete().should.be.true
+  })
+  it('Should throw an error when I want to add a fourth answer', () => {
+    // See Mocha Documentation - needs binding to be invoked correctly
+    test_event.answerQuestion.bind(test_event).should.throw(LunchEventError)
+  })
+  it('Should print the given answers', () => {
+    test_event.printData()[1].should.be.equal('2018-10-13')
+  })
 })
 
+describe('MysteryLunch', () => {
+  describe("Startup and echoing 'Hello'", () => {
+    var output = ''
+    var match = ''
 
-describe("MysteryLunch", () => {
-   describe("Startup and echoing 'Hello'", () => {
-      var output = "";
-   
-      it("should print 'Mystery Lunch Planner' when started", () => {
-         output = test_stdout.inspectSync( () => {
-            mystl.start();
-         })
-         match = /Mystery Lunch Planner/.test(output);
-         assert.ok(match);
-      });
-      
-      it("should show the prompt '$:'", () => {
-         match = /\$:/.test(output);
-         assert.ok(match);
-      });
-      it("should echo 'Hello!' when I enter 'Hello'", () => {
-         output = test_stdout.inspectSync( () => {
-            test_stdin.send('Hello!');
-         });
-         match = /\$>\ \'Hello!\'/.test(output);
-         assert.ok(match);
-      });  
-   })
-   describe("Managaging events", () => {
-      var output = "foo"
-      it("should start the creation of an event when I type 'manage events'", () => {
-         output = test_stdout.inspectSync( () => {
-            test_stdin.send('M');
-         });
-         match = /\$> Welcome to managing events\. What do you want to do\?/.test(output);
-         assert.ok(match);
+    it("should print 'Mystery Lunch Planner' when started", () => {
+      output = testStdout.inspectSync(() => {
+        mystl.start()
       })
-      it("should show the options 'Create New Event' and 'Show all events'", () => {
-         match = /Create new event/.test(output);
-         assert.ok(match);
+      match = /Mystery Lunch Planner/.test(output)
+      assert.ok(match)
+    })
 
-         match = /Show all events/.test(output);
-         assert.ok(match);
+    it("should show the prompt '$:'", () => {
+      match = /\$:/.test(output)
+      assert.ok(match)
+    })
+    it("should echo 'Hello!' when I enter 'Hello'", () => {
+      output = testStdout.inspectSync(() => {
+        testStdin.send('Hello!')
       })
-      it("should create a new event by asking me a set of questions", () => {
-         output = test_stdout.inspectSync( () => {
-            test_stdin.send('C');
-         });
-         match = /What is the name of the event\?/.test(output);
-         assert.ok(match);
-         
-         output = test_stdout.inspectSync( () => {
-            test_stdin.send('My First Mystery Lunch');
-            test_stdin.send('2018-11-05');
-         });
-         match = /Who is participating\?/.test(output);
-         assert.ok(match);
+      match = /\$> 'Hello!'/.test(output)
+      assert.ok(match)
+    })
+  })
+  describe('Managaging events', () => {
+    var output = ''
+    var match = ''
+    it("should start the creation of an event when I type 'manage events'", () => {
+      output = testStdout.inspectSync(() => {
+        testStdin.send('M')
       })
-      it("should print the 'Thank You' message when I finished the object creation", () => {
-          output = test_stdout.inspectSync( () => {
-            test_stdin.send('Sebastian, Janine');
-          })
-          match = /Thank you! The event is registered\./.test(output);
-          assert.ok(match);
-      })
-      it("should have an object representing the event", () => {
-         let event = mystl.lunch_events[0];
-         event.title.should.be.equal('My First Mystery Lunch');
-         event.date.should.be.equal('2018-11-05');
-         event.participants.should.be.equal('Sebastian, Janine');
+      match = /\$> Welcome to managing events\. What do you want to do\?/.test(output)
+      assert.ok(match)
+    })
+    it("should show the options 'Create New Event' and 'Show all events'", () => {
+      match = /Create new event/.test(output)
+      assert.ok(match)
 
-      });
-   })
+      match = /Show all events/.test(output)
+      assert.ok(match)
+    })
+    it('should create a new event by asking me a set of questions', () => {
+      output = testStdout.inspectSync(() => {
+        testStdin.send('C')
+      })
+      match = /What is the name of the event\?/.test(output)
+      assert.ok(match)
+
+      output = testStdout.inspectSync(() => {
+        testStdin.send('My First Mystery Lunch')
+        testStdin.send('2018-11-05')
+      })
+      match = /Who is participating\?/.test(output)
+      assert.ok(match)
+    })
+    it("should print the 'Thank You' message when I finished the object creation", () => {
+      output = testStdout.inspectSync(() => {
+        testStdin.send('Sebastian, Janine')
+      })
+      match = /Thank you! The event is registered\./.test(output)
+      assert.ok(match)
+    })
+    it('should have an object representing the event', () => {
+      let event = mystl.lunch_events[0]
+      event.title.should.be.equal('My First Mystery Lunch')
+      event.date.should.be.equal('2018-11-05')
+      event.participants.should.be.equal('Sebastian, Janine')
+    })
+  })
 })
