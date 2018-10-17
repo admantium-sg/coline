5b9f5f0e2ad1aa19f32596a7497e8f732a63b986
devcon <devcon@admantium.com>
2018-10-17 07:46:08 +0200

    Prepare merge with branch 'B_improve_project_structure'

---
+++ app.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Prepare merge with branch 'B_improve_project_structure'
+const MysteryLunch = require('./src/index.js').MysteryLunch
+const mysteryLunch = new MysteryLunch()
+
+mysteryLunch.start()
