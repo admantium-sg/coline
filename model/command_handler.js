d298b8f78c6e626e4420f7d2ae5bae8f52b58ae0
devcon <devcon@admantium.com>
2018-11-10 18:15:41 +0100

    Documentation for CommandHandler

--- model/command_handler.js
+++ model/command_handler.js
@@ -1,16 +1,44 @@
 const EventEmitter = require('events')
 
+/**
+ * CommandHandler is an EventEmitter. It receives commands, emits them to the registered Interface Onjects and Context Objects.
+ *
+ * During application statrtup, it dependency injects a writeCallback and itself to InterfaceObjects so that they registr their commands with the CommandHandler.
+ */
+
 class CommandHandler extends EventEmitter {
+
+  /**
+   *  Create a new instance with the following objects:
+   * * contextObject: An object representing the current dialof
+   * * interfaceObjects: The list of all objects that represent key => commands bindings
+   *
+   * @constructor
+   */
   constructor () {
     super()
     this.contextObject = null
     this.interfaceObjects = []
-    this.setContextObject = (object) => { this.contextObject = object }
-    this.resetContextObject = () => { this.contextObject = null }
+    this.setContextObject = (object) => {
+      this.contextObject = object
+    }
+    this.resetContextObject = () => {
+      this.contextObject = null
+    }
   }
 
+  /**
+   * Processes all commands.
+   * * If a context obejct is set, emit ``context`` and the cmd message
+   * * Else emit the cmd
+   * * Else invoke ``echo`` with the cmd
+   *
+   * @param {*} cmd The command to be executed
+   *
+   */
+
   process (cmd) {
-    // emit cmd, if not handled then echo
+    // Emit cmd, if not handled then echo
     if (this.contextObject !== null) {
       this.emit('context', cmd)
     } else if (!this.emit(cmd, cmd)) {
@@ -18,6 +46,11 @@ class CommandHandler extends EventEmitter {
     }
   }
 
+  /**
+   * Adds new interface objects and registers their commands with this CommandHandler
+   * @param {*} InterfaceObject The object that is added
+   * @param {*} writeCallback Dependency Inject the write callback to the
+   */
   registerInterfaceObject (InterfaceObject, writeCallback) {
     let iob = new InterfaceObject(this, writeCallback)
     this.interfaceObjects.push(iob)
@@ -25,4 +58,4 @@ class CommandHandler extends EventEmitter {
   }
 }
 
-module.exports = { CommandHandler }
+module.exports = {CommandHandler}
