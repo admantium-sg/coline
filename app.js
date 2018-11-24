d91002480f5025cfecda85d7421e459a8d537eaa
devcon <devcon@admantium.com>
2018-11-24 15:46:53 +0100

    Prepare merge with branch G_tests

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// Prepare merge with branch 07_persisting_events
+// Prepare merge with branch G_tests
 const CommandLineInterpreter = require('./model/index').CommandLineInterpreter
 const MysteryLunch = require('./app/mystery_lunch').MysteryLunch
 
